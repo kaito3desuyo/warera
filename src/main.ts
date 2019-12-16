b511b80133de89d4885fe86aacfa438805a98753
kaito3desuyo <admin@seapolis.me>
2019-12-16 18:37:50 +0900

    テスト

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { Warera } from "./classes/warera";
 import { IDateOfJapaneseCalendar } from "./interfaces/i-date-of-japanese-calendar";
 
 function warera(date: IDateOfJapaneseCalendar | Date): string {
   return "hoge";
+  /*
   if (date instanceof Date) {
     return Warera.createFromDate(date);
   } else {
     return Warera.create(date);
   }
+   */
 }
 
 export default warera;
