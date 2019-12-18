930a3e66d274b18add0d5f851b6415e5992c71ba
up-tri <contact@up-tri.me>
2019-12-18 18:33:46 +0900

    methods of export was changed.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Warera } from "./classes/warera";
 import { IWareraInput } from "./interfaces/i-warera-input";
 
-function warera(date: IWareraInput | Date): Warera {
+export default function warera(date: IWareraInput | Date): Warera {
   if (date instanceof Date) {
     return Warera.createFromDate(date);
   } else if (date instanceof Object) {
@@ -10,5 +10,3 @@ function warera(date: IWareraInput | Date): Warera {
     throw new Error("Invalid argument.");
   }
 }
-
-export default warera;
