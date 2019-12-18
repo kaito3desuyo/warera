0544cd8db950ce6370b5b3c1075b29e9304a3ae3
kaito3desuyo <admin@seapolis.me>
2019-12-18 09:50:26 +0900

    微修正

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { IWareraInput } from "./interfaces/i-warera-input";
 function warera(date: IWareraInput | Date): Warera {
   if (date instanceof Date) {
     return Warera.createFromDate(date);
-  } else (date instanceof Object) {
+  } else if (date instanceof Object) {
     return Warera.createFromWareraInput(date);
+  } else {
+    throw new Error("Invalid argument.");
   }
 }
 
