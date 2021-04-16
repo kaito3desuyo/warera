653c7a9a77cf3de7bc555e0906ce6433dd8f4548
kaito3desuyo <admin@seapolis.me>
2021-04-16 19:36:26 +0900

    Fix enum to union

--- src/enums/japanese-calendar-era.enum.ts
+++ src/enums/japanese-calendar-era.enum.ts
@@ -1,7 +1,8 @@
-export const enum EJapaneseCalendar {
-  REIWA = "R",
-  HEISEI = "H",
-  SHOWA = "S",
-  TAISHO = "T",
-  MEIJI = "M"
-}
+export const EJapaneseCalendar = {
+  REIWA: "R",
+  HEISEI: "H",
+  SHOWA: "S",
+  TAISHO: "T",
+  MEIJI: "M",
+};
+export type EJapaneseCalendar = typeof EJapaneseCalendar[keyof typeof EJapaneseCalendar];
